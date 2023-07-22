7f9338edcf196ab4e13a6e5b37d0b22ec9c69c09
Marco Almeida <marco.almeida@ua.pt>
2023-07-22 17:27:51 +0100

    producer might be ready

--- data-gen/producer.py
+++ data-gen/producer.py
@@ -47,20 +47,9 @@ if __name__ == "__main__":
     stations = [Station(i) for i in range(5)]
 
     while True:
-        time.sleep(randint(1, 5))
+        time.sleep(randint(0, 5))
         # choose random station
         station = stations[randint(0, len(stations) - 1)]
-        data = station.generate_data()
-        producer.produce("weather_station-temperature", key="real", value=json.dumps(data), callback=acked)
-        send_data()
-        # producer.produce("weather_station-temperature", key="apparent", value=str(25 + randint(-5, 5)), callback=acked)
-        # producer.produce("weather_station-temperature", key="real", value=str(20 + randint(-5, 10)), callback=acked)
-        producer.produce("weather_station-temperature", key="real", value=json.dumps({"cena": 1, "ts": timestamp}), callback=acked)
-        # producer.produce("weather_station-wind", key="speed", value=str(60 + randint(-40, 20)), callback=acked)  # km/h
-        # producer.produce("weather_station-wind", key="direction", value=str(randint(0, 360)), callback=acked)  # °
-        # producer.produce("weather_station-humidity", key="default", value=str(60 + randint(-10, 20)), callback=acked)  # %
-        # producer.produce("weather_station-pressure", key="default", value=str(1015 + randint(-5, 5)), callback=acked)  # hPa
-        # producer.produce("weather_station-precipitation", key="default", value=str(randint(0, 10)), callback=acked)  # mm
-        # producer.produce("weather_station-radiation", key="uv", value=str(randint(1, 11)), callback=acked)  #
-        # producer.produce("weather_station-radiation", key="solar", value=str(randint(100, 600)), callback=acked)  # mm
+        topic, data = station.generate_data()
+        producer.produce(f"weather_station-{topic}", key="real", value=json.dumps(data), callback=acked)
         producer.poll(1)
