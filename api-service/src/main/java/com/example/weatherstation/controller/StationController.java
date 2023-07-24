631d8d1c3d59b9cff45327f1ba29945d5094ddb7
Marco Almeida <marco.almeida@ua.pt>
2023-07-24 15:44:33 +0100

    all stations endpoint

--- api-service/src/main/java/com/example/weatherstation/controller/StationController.java
+++ api-service/src/main/java/com/example/weatherstation/controller/StationController.java
@@ -2,28 +2,35 @@ package com.example.weatherstation.controller;
 
 import com.example.weatherstation.model.Station;
 import com.example.weatherstation.service.StationService;
-import jakarta.websocket.server.PathParam;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
-@RequestMapping("api/v1/station")
+@RequestMapping("api/v1")
 public class StationController {
 
     @Autowired
     private StationService stationService;
 
-    @GetMapping("/{station_id}")
+    @GetMapping("/station/{station_id}")
     public ResponseEntity<Station> getStationStuff(@PathVariable("station_id") long stationId) {
         Station s = stationService.getStation(stationId);
         return new ResponseEntity<>(s, HttpStatus.OK);
     }
 
-    @PostMapping("/{station_id}")
+    @GetMapping("/stations")
+    public ResponseEntity<List<Long>> getAllStationIds() {
+        List<Long> s = stationService.getAllStationIds();
+        return new ResponseEntity<>(s, HttpStatus.OK);
+    }
+
+    @PostMapping("/station/{station_id}")
     public ResponseEntity<Station> createStation(@PathVariable("station_id") long stationId) {
         Station s = stationService.createStation(stationId);
         return new ResponseEntity<>(s, HttpStatus.CREATED);
