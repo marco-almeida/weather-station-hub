2d6f081c6a4a271f18e84933247a6299cfe114f3
Marco Almeida <marco.almeida@ua.pt>
2023-07-24 14:40:33 +0100

    models

---
+++ api-service/src/main/java/com/example/weatherstation/model/Measurement.java
@@ -0,0 +1,27 @@
+package com.example.weatherstation.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Table(name = "measurement")
+public class Measurement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long timestamp;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private double value;
+}
---
+++ api-service/src/main/java/com/example/weatherstation/model/Station.java
@@ -0,0 +1,23 @@
+package com.example.weatherstation.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "station")
+public class Station {
+    @Id
+    private Long id;
+
+    @OneToMany(cascade = {CascadeType.ALL})
+    private List<Measurement> measurements;
+}
