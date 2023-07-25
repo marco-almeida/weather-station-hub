039865ec6a312cf19702c4785a6ce2b064ce43ad
Marco Almeida <marco.almeida@ua.pt>
2023-07-25 16:11:25 +0100

    producer changes for single values only

--- data-gen/station.py
+++ data-gen/station.py
@@ -12,43 +12,47 @@ class Station:
 
     def generate_data(self):
         functions = [
-            (self.generate_temperature_data, "temperature"),
-            (self.generate_wind_data, "wind"),
+            (self.generate_real_temperature_data, "real_temperature"),
+            (self.generate_apparent_temperature_data, "apparent_temperature"),
+            (self.generate_wind_direction_data, "wind_direction"),
+            (self.generate_wind_speed_data, "wind_speed"),
             (self.generate_humidity_data, "humidity"),
             (self.generate_pressure_data, "pressure"),
             (self.generate_precipitation_data, "precipitation"),
-            (self.generate_radiation_data, "radiation"),
+            (self.generate_solar_radiation_data, "solar_radiation"),
+            (self.generate_uv_data, "uv"),
         ]
         func, topic = functions[randint(0, len(functions) - 1)]
         payload = func()
-        return topic, {"station_id": self.id, "timestamp": int(time.time()), "payload": payload}
+        return topic, {"station_id": self.id, "timestamp": int(time.time()), "type": topic, "payload": payload}
 
-    def generate_temperature_data(self):
+    def generate_real_temperature_data(self):
         apparent = 25 + randint(-self.variation, self.variation)  # °C
-        real = apparent - randint(-self.variation, self.variation)  # °C
-        return {"apparentTemperature": apparent, "realTemperature": real}
+        return apparent - randint(-self.variation, self.variation)  # °C
 
-    def generate_wind_data(self):
-        wind_speed = 20 + randint(-self.variation, self.variation * 8)  # km/h
-        wind_direction = randint(0, 360)  # °
-        return {"windSpeed": wind_speed, "windDirection": wind_direction}
+    def generate_apparent_temperature_data(self):
+        return 25 + randint(-self.variation, self.variation)  # °C
+
+    def generate_wind_speed_data(self):
+        return 20 + randint(-self.variation, self.variation * 8)  # km/h
+
+    def generate_wind_direction_data(self):
+        return randint(0, 360)  # °
 
     def generate_humidity_data(self):
-        humidity = 60 + randint(-self.variation, self.variation * 2)  # %
-        return {"humidity": humidity}
+        return 60 + randint(-self.variation, self.variation * 2)  # %
 
     def generate_pressure_data(self):
-        pressure = 1015 + randint(-self.variation, self.variation)  # hPa
-        return {"pressure": pressure}
+        return 1015 + randint(-self.variation, self.variation)  # hPa
 
     def generate_precipitation_data(self):
-        precipitation = randint(0, 10)  # mm
-        return {"precipitation": precipitation}
+        return randint(0, 10)  # mm
 
-    def generate_radiation_data(self):
-        uv = randint(1, 11)  # scale
-        solar = 300 + randint(-self.variation * 20, self.variation * 20)  # scale
-        return {"uv": uv, "solar": solar}
+    def generate_solar_radiation_data(self):
+        return 300 + randint(-self.variation * 20, self.variation * 20)  # scale
+
+    def generate_uv_data(self):
+        return randint(1, 11)  # scale
 
     def __str__(self) -> str:
         return f"Station {self.id}"
