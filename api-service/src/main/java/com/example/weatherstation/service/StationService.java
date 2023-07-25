66369c045e88029bb6cbba7d3cb7f0903996ebc0
Marco Almeida <marco.almeida@ua.pt>
2023-07-25 15:19:46 +0100

    already exists exception

--- api-service/src/main/java/com/example/weatherstation/service/StationService.java
+++ api-service/src/main/java/com/example/weatherstation/service/StationService.java
@@ -1,5 +1,6 @@
 package com.example.weatherstation.service;
 
+import com.example.weatherstation.exception.ResourceAlreadyExistsException;
 import com.example.weatherstation.exception.ResourceNotFoundException;
 import com.example.weatherstation.model.Station;
 import com.example.weatherstation.repository.StationRepository;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class StationService {
@@ -17,7 +19,14 @@ public class StationService {
         return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource not Found!"));
     }
 
+    public List<Long> getAllStationIds() {
+        return repository.findAll().stream().map(Station::getId).toList();
+    }
+
     public Station createStation(Long id) {
+        if (repository.existsById(id)) {
+            throw new ResourceAlreadyExistsException(String.format("Station with id %d already exists!", id));
+        }
         return repository.save(new Station(id, new ArrayList<>()));
     }
 }
